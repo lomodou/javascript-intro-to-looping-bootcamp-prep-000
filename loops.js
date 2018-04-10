ea63b0d81804bb69ed0584e21336f863834c5015
Modou Lo <modoulo99@icloud.com>
2018-04-10 05:48:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -32,3 +32,14 @@ function whileLoop(number) {
 }
 
 
+function doWhileLoop(array) {
+  
+  do {
+    
+    array.pop();
+    
+  }while (array.length > 0 && maybeTrue());
+  
+  return array;
+  
+}
